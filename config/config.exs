9268cdb937318ed6a51e9e5209418fa796c4b648
Konstantin Astafurov <konstantinaa@Artyoms-MacBook-Pro-2.local>
2022-03-31 16:30:08 -0400

    bruh I keep leaking my token

--- config/config.exs
+++ config/config.exs
@@ -2,5 +2,5 @@ import Config
 
 config :nostrum,
   # The token of your bot as a string
-  token: "OTU3NjkxMjM0NzQ5NjQ0ODUw.YkCddw.SNDbCowj43SiyTP5To5UkYqylb0",
+  token: "",
   gateway_intents: :all
