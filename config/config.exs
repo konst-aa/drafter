ff8599e9f6c630ebb4814004fd594957fd3e779c
Konstantin Astafurov <konstantinaa@Artyoms-MacBook-Pro-2.local>
2022-03-30 21:28:38 -0400

    working prototype, needs configuring of admin perms and deregestering stuff

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,5 @@
 import Config
 
 config :nostrum,
-  token: "OTU3NjkxMjM0NzQ5NjQ0ODUw.YkCddw.VliMtChuYLLDps02a20ZSssoXy4",  # The token of your bot as a string
+  token: "",  # The token of your bot as a string
   gateway_intents: :all
